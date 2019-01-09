0fd2d565015bafacb57e2d2d1f09311cae819166
Andrea Rodrigues <andrea.melissa.rodrigues@gmail.com>
2019-01-09 17:49:42 +0000

    Update weight_maintenance_program.py

--- weight_maintenance_program.py
+++ weight_maintenance_program.py
@@ -1,118 +1,200 @@
+# WEIGHT MAINTENANCE PROGRAM
+
+#IMPORTS
 import sys
 
-def inputAge(message):
+def finalOutput(userData):
+    currentBMR = userData[5]
+    currentBMI = userData[7]
+    standardBMICategory = userData[8]
+    dailyCalorieIntake = userData[6]
+    print("\nHere is you data:\n\nCurrent BMR = ", currentBMR,
+      "\nCurrent BMI = ", currentBMI,
+      "\nTarget BMI = 22",
+      "\nStandard BMI Cateogory: ", standardBMICategory,
+      "\nDaily Calorie Intake = ", dailyCalorieIntake)
+    
+    print("\nThanks for using the Weight Maintenance Program.\n")
+    quit()
+
+def bmiCategories(userData):
+    # Standard BMI Categories
+    currentBMI = userData[7]
+    if currentBMI < 18.5:
+        standardBMICategory = 'Underweight'
+        'Normal weight'
+        'Overweight'
+        'Obese'
+    elif currentBMI >= 18.5 and currentBMI <= 24.9:
+        standardBMICategory = 'Normal weight'
+    elif currentBMI >= 25 and currentBMI <= 29.9:
+        standardBMICategory = 'Overweight'
+    elif currentBMI > 30:
+        standardBMICategory = 'Obese'
+    else:
+        standardBMICategory = 'cannot be calculated'
+    userData.append(standardBMICategory)
+    return userData
+
+
+def calculations(userData):
+    userHeight = userData[2]
+    userWeight = userData[3]
+    levelOfExercise = userData[4]
+    currentBMR = userData[5]
+
+    dailyCalorieIntake = currentBMR * levelOfExercise
+    userData.append(dailyCalorieIntake)
+
+    currentBMI = userWeight / (userHeight * userHeight)
+    currentBMI = round(currentBMI,1)
+    userData.append(currentBMI)
+    return userData
+
+def checkGender(userData):
+    # extract data
+    userGender = userData[1]
+    userHeight = userData[2]
+    userWeight = userData[3]
+    # check Male/Female BMI
+    if userGender == 'm':
+        currentBMR = 88.362 + (13.397 * userWeight) + (4.799 * userHeight)
+        currentBMR = round(currentBMR, 2)
+        userData.append(currentBMR)
+        return currentBMR
+    else:
+        currentBMR = 447.593 + (9.247 * userWeight) + (3.098 * userHeight)
+        currentBMR = round(currentBMR, 2)
+        userData.append(currentBMR)
+        return currentBMR
+
+def inputAmountExercise(userData):
     while True:
         try:
-            userInput = int(input(message))
+            amountExercise = int(input('\nEnter the number of times you do exercise per week:\t'))
+        except ValueError:
+            print ("That is not valid. Try again.")
+        else:
+            if amountExercise == "":
+                print("You have not entered anything. Please enter an amount.")
+            else:
+                if amountExercise < 1:
+                    levelOfExercise = 1.2
+                    userData.append(levelOfExercise)
+                    return userData
+                elif amountExercise >= 1 and amountExercise <= 3:
+                    levelOfExercise = 1.375
+                    userData.append(levelOfExercise)
+                    return userData
+                elif amountExercise == 4 or amountExercise == 5:
+                    levelOfExercise = 1.55
+                    userData.append(levelOfExercise)
+                    return userData
+                elif amountExercise == 6 or amountExercise == 7:
+                    levelOfExercise = 1.725
+                    userData.append(levelOfExercise)
+                    return userData
+                elif amountExercise >= 8:
+                    levelOfExercise = 1.9
+                    userData.append(levelOfExercise)
+                    return userData
+                else:
+                    print("Sorry that is not valid, please try again.")
+
+def inputWeight(userData):
+    while True:
+        try:
+            userWeight = float(input('\nEnter your weight (kg):\t'))
+        except ValueError:
+            print ("That is not a valid weight. Try again.")
+        else:
+            if userWeight == "":
+                print("You have not entered anything. Please enter your weight (number).")
+            else:    
+                if userWeight >= 30 and userWeight <= 250:
+                    userData.append(userWeight)
+                    return userData
+                else:
+                    print("Sorry!")
+
+
+def inputHeight(userData):
+    while True:
+        try:
+            userHeight = float(input('\nEnter your height (m):\t'))
+        except ValueError:
+            print ("That is not a valid height. Try again.")
+        else:
+            if userHeight == "":
+                print("You have not entered anything. Please enter your height (number).")
+            else:
+                if userHeight > 1.20 and userHeight < 2.10:
+                    userData.append(userHeight)
+                    return userData
+                else:
+                    print("Sorry!")
+
+def inputGender(userData):
+    while True:
+        try:
+            userGender = input('\nEnter your gender (M)ale or (F)emale:\t')
+            userGender = userGender.lower()
+        except ValueError:
+            print("Sorry your input is not valid. Please try again.")
+        else:
+            ##check gender
+            if userGender == 'm' or userGender == 'male':
+                userGender = 'm'
+                userData.append(userGender)
+                return userData
+            elif userGender == 'f' or userGender == 'female':
+                userGender = 'f'
+                userData.append(userGender)
+                return userData
+            else:
+                print('That is not a valid input. Please try again.')
+
+def inputAge():
+    while True:
+        try:
+            userAge = int(input("Please enter your age:\t"))
         except ValueError:
             print("That is not a valid age. Try again.")
-            continue
         else:
-            return userInput
+            #check age
+            if userAge == "":
+                print("You have not entered anything. Please enter a number.")
+            elif userAge >= 14 and userAge <= 100:#14 and 100 is inclusive this was interpreted from the spec
+                userData = [userAge]
+                return userData
+            else:
+                print('You are not within the Membership age range')
+                sys.exit()# ends the program if the user is not withing the membership age range
 
-def inputGender():
-    userGender = input('\nEnter your gender (M)ale or (F)emale:\t')
-    userGender = userGender.lower()
-    ##check gender
-    isGenderCorrect = False
-    while isGenderCorrect == False:
-        if userGender == 'm' or userGender == 'male':
-            userGender = 'm'
-            isGenderCorrect = True
-        elif userGender == 'f' or userGender == 'female':
-            userGender = 'f'
-            isGenderCorrect = True
+
+def welcomeMessage():
+    print("--------------------------------------------------")
+    print("----Welcome to the Weight Maintenance Program!----")
+    print("--------------------------------------------------")
+    try:
+        continueProgram = input("Press ENTER to continue...")
+    except ValueError:
+        print("Sorry that is not a valid input. Try Again.")
+    else:
+        if continueProgram == "":
+            print("Starting Program...")
+            userData = inputAge()
+            userData = inputGender(userData)
+            userData = inputHeight(userData)
+            userData = inputWeight(userData)
+            levelOfExercise = inputAmountExercise(userData)
+            currentBMR = checkGender(userData)
+            userData = calculations(userData)
+            standardBMICategory = bmiCategories(userData)
+            finalOutput(userData)
         else:
-            userGender = input('That is not a valid input. Please enter your gender (M)ale or (F)emale:\t')
-            userGender = userGender.lower()
-    return userGender
+            print("Thanks for visiting. Bye!")
 
+# MAIN CODE
 
-# MAIN PROGRAM STARTS HERE:
-userAge = inputAge("Enter your age:\n")
-#check age
-if userAge >= 14 and userAge <= 100:#14 and 100 is inclusive this was interpreted from the spec
-    isAgeCorrect = True
-    userGender = inputGender()
-else:
-    print('You are not within the Membership age range')
-    sys.exit()#ends the program if the user is not withing the membership age range
-
-
-userHeight = float(input('\nEnter your height (m):\t'))
-##check height
-isHeightCorrect = False
-while isHeightCorrect == False:
-    if userHeight > 1.20 and userHeight < 2.10:
-        #print('isHeightCorrect = True')
-        isHeightCorrect = True
-    else:
-        userHeight = float(input('That is not valid. Enter your height (m):\t'))
-
-
-userWeight = float(input('\nEnter your weight (kg):\t'))
-##check weight
-isWeightCorrect = False
-while isWeightCorrect == False:
-    if userWeight >= 30 and userWeight <= 250:
-        isWeightCorrect = True
-    else:
-        userWeight = float(input('That is not valid Enter your weight (kg):\t'))
-
-
-amountExercise = int(input('\nEnter the number of times you do exercise per week:\t'))
-isAmountExerciseCorrect = False
-while isAmountExerciseCorrect == False:
-    isAmountExerciseCorrect = True
-    if amountExercise < 1:
-        levelOfExercise = 1.2
-    elif amountExercise >= 1 and amountExercise <= 3:
-        levelOfExercise = 1.375
-    elif amountExercise == 4 or amountExercise == 5:
-        levelOfExercise = 1.55
-    elif amountExercise == 6 or amountExercise == 7:
-        levelOfExercise = 1.725
-    elif amountExercise >= 8:
-        levelOfExercise = 1.9
-    else:
-        isAmountExerciseCorrect = False
-        amountExercise = int(input('Enter the number of times you do exercise per week:\t'))
-
-
-##check Male/Female BMI
-if userGender == 'm':
-    currentBMR = 88.362 + (13.397 * userWeight) + (4.799 * userHeight)
-    currentBMR = round(currentBMR, 2)
-else:
-    currentBMR = 447.593 + (9.247 * userWeight) + (3.098 * userHeight)
-    currentBMR = round(currentBMR, 2)
-
-
-dailyCalorieIntake = currentBMR * levelOfExercise
-currentBMI = userWeight / (userHeight * userHeight)
-currentBMI = round(currentBMI,1)
-
-
-##Standard BMI Categories
-if currentBMI < 18.5:
-    standardBMICategory = 'Underweight'
-    'Normal weight'
-    'Overweight'
-    'Obese'
-elif currentBMI >= 18.5 and currentBMI <= 24.9:
-    standardBMICategory = 'Normal weight'
-elif currentBMI >= 25 and currentBMI <= 29.9:
-    standardBMICategory = 'Overweight'
-elif currentBMI > 30:
-    standardBMICategory = 'Obese'
-else:
-    standardBMICategory = 'cannot be calculated'
-
-
-
-##Final Output
-print('\nCurrent BMR = ', currentBMR,
-      '\nCurrent BMI =', currentBMI,
-      '\nTarget BMI = 22',
-      '\nStandard BMI Cateogory: ', standardBMICategory,
-      '\nDaily Calorie Intake =', dailyCalorieIntake)
+welcomeMessage()
